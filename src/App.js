c3072294e4abf546a3b03c0e6b5597bb9f97922f
anushkax1 <144704315+anushkax1@users.noreply.github.com>
2023-10-14 21:31:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import { useState } from 'react';
-import './App.css';
-function App() {
-  const [value, setValue] = useState('');
-  return (
-    <div className="calculator-grid">
-      <div className="output">
-        <input type="text" value={value}/>
-      </div>
+import React from 'react';
+import UserTable from './UserTable';
 
-      <input type="button" className="span-two" value="AC" onClick={e=> setValue('')}/>
-<input type="button" value="DEL" onClick={e=> setValue(value.slice(0, -1))}/>
-<input type="button" value="+" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="1" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="2" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="3" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="/" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="4" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="5" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="6" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="+" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="7" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="8" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="9" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="-" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="0" onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" value="." onClick={e=> setValue(value + e.target.value)}/>
-<input type="button" className="span-two" value="=" onClick={e=> setValue(eval(value))}/>
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
